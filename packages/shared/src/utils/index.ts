0d85bdb505a263334359f0bcd53dc513936f7454
ajiu9 <615944323@qq.com>
2024-04-12 15:03:42 +0800

    feat: add utils function of promiseTimeout

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -1,2 +1,18 @@
 export * from './is'
 export * from './types'
+
+
+
+export function promiseTimeout(
+  ms: number,
+   throwOnTimeout: false,
+    reason = 'Timeout'
+  ): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if (throwOnTimeout) {
+      setTimeout(()=>reject(reason),ms)
+    }
+    else
+    setTimeout(resolve,ms)
+  })
+}
