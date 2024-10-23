d3c975e3daf49cbdd3a592001f8245fb00f6b77b
ajiu9 <615944323@qq.com>
2024-10-23 10:59:03 +0800

    chore: lint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 import ajiu9 from '@ajiu9/eslint-config'
 
 export default ajiu9({
-  ignores: ['packages/.vitepress/cache'],
+  formatters: true,
+}, {
+  ignores: [
+    '**/cache',
+    '**/dist',
+  ],
 })
